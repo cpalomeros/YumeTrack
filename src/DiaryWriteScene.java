437d5aeaf1ed2e5f6cfc7c32138716d0bbfdab5f
nhyume <cjpalsan705@gmail.com>
2026-07-26 15:16:16 +0200

    NOW IT SAVES FILES???

--- src/DiaryWriteScene.java
+++ src/DiaryWriteScene.java
@@ -7,8 +7,13 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.geometry.Insets;
+import java.io.File;
+import java.io.FileWriter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.io.IOException;
+
+
 
 
 public class DiaryWriteScene {
@@ -45,10 +50,43 @@ public class DiaryWriteScene {
 
         // Button actions
         saveButton.setOnAction(e -> {
+
+            File folder = new File("Diary");
+
+            if (!folder.exists()) {
+            folder.mkdir();
+            };
+
+            
             String entryTitle = writingField.getText();
+            String safeDate = today.replaceAll("/", "-");
+            String safeTitle = entryTitle.replaceAll("[\\\\/:*?\"<>|]", "_");
             String entryText = writingArea.getText();
+            String filename = "Diary/" + safeDate + "-" + safeTitle + ".txt";
+
+            
+            try {
+    FileWriter writer = new FileWriter(filename);
+    
+
+
+            writer.write("Date:" + today + "\n");
+            writer.write("Title:" + entryTitle + "\n\n");
+            writer.write(entryText);
+
+            writer.close();
+
+    writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
             // Save the entry 
-            System.out.println("Entry saved: " + entryTitle + entryText);
+            System.out.println("Entry saved: " + safeTitle + entryText);
+
+
             stage.setScene(DiaryMenuScene.createScene(stage)); // Go back to main scene
         });
 
